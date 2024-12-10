918494521375d58094d95900b0a276f8fb35cc4b
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-10 23:10:15 +0530

    fix: user route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,19 @@ import express from 'express'
 import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from './config/db.js'
+import uesrRoutes from './routes/user.routes.js'
 dotenv.config()
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 connectDB();
 app.get('/', (req, res) => {
   res.send("hello")
 })
+
+app.use('/users', uesrRoutes);
 
 export default app;
