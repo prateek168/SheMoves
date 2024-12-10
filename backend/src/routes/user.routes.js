e72d541a2acf4e193ba887b071f95a99237c3f48
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-10 23:09:40 +0530

    implement user register  route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import { registerUser } from '../controllers/user.controller';
 const router = express.Router()
 
-router.post('/register')
+router.post('/register' , registerUser)
 
 
 
