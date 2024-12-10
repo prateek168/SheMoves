40a2608ad0d418403447660905385e925c71ed9e
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-10 23:08:34 +0530

    implement user register

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,11 +1,30 @@
 import userModel from "../models/user.model";
+import generateToken from "../utils/jwt";
 import { userSchema } from "../validations/user.validation";
-
-
-export const Register = aysnc ( req , res ) =>{
+import bcrypt from 'bcrypt'
+import * as userService from '../services/user.service'
+export const registerUser = async ( req , res , next )=> {
  try{
-    const validatedData = userSchema.parse(req.body);
-    
+    const {firstname , lastname , email , password} = userSchema.parse(req.body);
+   if (!email || !firstname || !password) {
+     return res.json(400).json({message:"All fields are mandatory"})
+   }
+     let user = await userModel.findOne({username})
+     if(user){
+      return res.status(400).json({message:"Username already Exists"})
+     }
+     const hashedPassword = await bcrypt.hash(password, 10);
+
+     // calling createUser function from userService
+      user = await userService.createUser({
+      firstname ,
+      lastname,
+      email,
+      password : hashedPassword
+    })
+
+    const token = generateToken(user._id);
+    res.status(201).json({user , token })
  }
  catch(error){
   console.log(error);
