f86c8e735fc078531012ca15424394f7ba65f3b6
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-12 11:49:34 +0530

    fix user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -6,18 +6,15 @@ const userSchema = new  mongoose.Schema({
      firstname: {
       type: String,
       required: true,
-      minlength: [3 , 'Firstname mmust be at least 3 characters']
      },
      lastname: {
       type: String,
-      minlength: [3 , 'Lastname must be atleast 3 characters']
      }
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    minlength: [5, 'email must be atleast 5 characters']
   },
   password: {
     type:String,
