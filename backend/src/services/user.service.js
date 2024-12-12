345cb7395a896ef04d0e597b948fcc9d0e37a132
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-12 11:51:00 +0530

    add user register

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,17 +1,18 @@
-import userModel from "../models/user.model";
+import userModel from "../models/user.model.js";
 
-export const createUser = async ( data )=>{
-  try { const user = await userModel.create({
-    fullname :{
-      firstname : data.firstname ,
-      lastname : data.lastname ,
-    },
-    email : data.email,
-    password : data.password,
-  })
-  return user;
-}
-catch(error){
-  console.log("Error while creating User " + error.message );
-}
+export const createUser = async (data) => {
+  try {
+    const user = await userModel.create({
+      fullname: {
+        firstname: data.firstname,
+        lastname: data.lastname,
+      },
+      email: data.email,
+      password: data.password,
+    });
+    return user;
+  } catch (error) {
+    console.error("Error while creating User:", error.message);
+    throw new Error("Failed to create user");
+  }
 };
