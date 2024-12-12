f4e37e85c209b9861022c3f525141896c03e1be8
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-12 22:17:42 +0530

    implement and import cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from './config/db.js'
 import uesrRoutes from './routes/user.routes.js'
+import cookieparser from "cookie-parser"
 dotenv.config()
 const app = express();
 
 app.use(cors());
+app.use(cookieparser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-
 connectDB();
 app.get('/', (req, res) => {
   res.send("hello")
