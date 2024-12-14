5e31a7e2b95106f0cc9ae04b1b99aa2acc463e32
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-14 21:10:43 +0530

    implement captain routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,20 +2,21 @@ import express from 'express'
 import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from './config/db.js'
-import uesrRoutes from './routes/user.routes.js'
+import userRoutes from './routes/user.routes.js'
 import cookieparser from "cookie-parser"
+import captainRoutes from './routes/captain.routes.js'
 dotenv.config()
 const app = express();
 
 app.use(cors());
 app.use(cookieparser())
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 connectDB();
 app.get('/', (req, res) => {
   res.send("hello")
 })
 
-app.use('/users', uesrRoutes);
-
+app.use('/users', userRoutes);
+app.use('/captains',captainRoutes);
 export default app;
