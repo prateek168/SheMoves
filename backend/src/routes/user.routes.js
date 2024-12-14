4139cc2a2bf0ae1f358362f0a2176cbc2c1356bf
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-14 14:22:01 +0530

    implement user logout routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { registerUser } from '../controllers/user.controller.js';
+import { getUserProfile, loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
+import { isAuthenticatedUser } from '../middleware/auth.middleware.js'
 const router = express.Router()
 
 router.post('/register' , registerUser)
-
-
+router.post('/login' , loginUser)
+router.get('/profile' , isAuthenticatedUser , getUserProfile )
+router.get('/logout' , isAuthenticatedUser , logoutUser)
 
 export default router;
