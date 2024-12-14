34482faeed0f5f91620f5cb21fa015dfdfe1eaf9
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-14 14:22:39 +0530

    fix jwt token middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import userModel from '../models/user.model.js';
+import blacklistTokenModel from '../models/blacklistToken.model.js';
 
 export const  isAuthenticatedUser = async (req, res, next)=>{
  try{ 
@@ -10,13 +11,16 @@ export const  isAuthenticatedUser = async (req, res, next)=>{
       message: "Token Not Found"
     })
   }
+  const isBlacklistToken = await blacklistTokenModel.findOne({token})
+  if(isBlacklistToken){
+    res.status(401).json({message:'Unauthorized'})
+  }
   const decode = await jwt.verify(token, process.env.JWT_SECRET);
   if(!decode){
     return res.status(401).json({
       message:"Invalid Token"
     })
   }
-  console.log(decode)
   const user = await userModel.findOne({ _id : decode.id} );
   if(!user) return res.json({message:"User not found with token"})
   req.user = user;
