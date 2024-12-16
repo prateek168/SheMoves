b6367455d363358671bb045e36b026eef1745de8
Prateek Verma <prateekverma1681998@gmail.com>
2024-12-16 18:45:48 +0530

    fix json web token

--- backend/src/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken'
-const secretKey = process.env.JWT_SECRET
-
-const generateToken = (payload , expiresIn ='1d') =>{
-  return jwt.sign(payload , secretKey , {expiresIn})
-}
-
-export default generateToken;
